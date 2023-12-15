51acd482685e61b90e48911dc380ba9937997746
Medhansh Jain <medhanshjain2010@gmail.com>
2023-12-15 21:06:45 +0530

    Added on click cart dropdown and cart context

--- clothing-app/src/components/cart-icon/cart-icon.component.jsx
+++ clothing-app/src/components/cart-icon/cart-icon.component.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import "./cart-icon.styles.scss";
 import { ReactComponent as CartLogo } from "../../assets/shopping-bag.svg";
+import { CartContext } from "../../contexts/cart.context";
+
 const CartItem = () => {
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
   return (
-    <div className="cart-icon-container">
+    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <CartLogo className="shopping-icon" />
       <span className="item-count">0</span>
     </div>
--- clothing-app/src/routes/navigation/navigation.component.jsx
+++ clothing-app/src/routes/navigation/navigation.component.jsx
@@ -4,10 +4,13 @@ import { ReactComponent as CrwnLogo } from "../../assets/crwn.svg";
 import CartItem from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import "./navigation.styles.scss";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -30,7 +33,7 @@ const Navigation = () => {
           )}
           <CartItem />
         </div>
-        <CartDropdown />
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
