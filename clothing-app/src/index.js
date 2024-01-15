bcc81404c28dabad5e53c70237b3958ebc2212ca
Medhansh Jain <medhanshjain2010@gmail.com>
2024-01-16 00:12:46 +0530

    addded categories component to routes

--- clothing-app/src/index.js
+++ clothing-app/src/index.js
@@ -1,26 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
 import { UserProvider } from "./contexts/user.context";
-import { ProductsProvider } from "./contexts/products.context";
+import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
-import reportWebVitals from "./reportWebVitals";
+
 import "./index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const rootElement = document.getElementById("root");
+
+render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  rootElement
 );
-
-reportWebVitals();
