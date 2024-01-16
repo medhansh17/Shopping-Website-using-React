5b13f19e0584b3460e5c3f3a4d5caffbf5e08cdc
Medhansh Jain <medhanshjain2010@gmail.com>
2024-01-16 16:19:38 +0530

    added links from home page

--- clothing-app/src/components/directory-item/directory-item.component.jsx
+++ clothing-app/src/components/directory-item/directory-item.component.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./directory-item.styles.scss";
 
 const DirectoryItem = ({ category }) => {
@@ -10,10 +11,10 @@ const DirectoryItem = ({ category }) => {
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className="body">
+      <Link className="body" to={`shop/${title}`}>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
+      </Link>
     </div>
   );
 };
